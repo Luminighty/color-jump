6ae2dd2762cbed66ce7cc6425ef360b6c8b186bf
Luminighty <balazs922@gmail.hu>
2019-08-03 21:50:34 +0200

    Prefabs, Goal, Menu

---
+++ Color Jump/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	public void StartLevel(int level) {
+		SceneManager.LoadScene("level " + level);
+	}
+
+	public void ExitGame() {
+		Application.Quit();
+	}
+}
