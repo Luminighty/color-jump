3da2dd77a47fafbbc5571e197e1c188bf8cd2acd
Luminighty <balazs922@gmail.hu>
2019-08-04 23:24:49 +0200

    UI animations

--- Color Jump/Assets/Scripts/UI/Slot.cs
+++ Color Jump/Assets/Scripts/UI/Slot.cs
@@ -4,8 +4,31 @@ using UnityEngine;
 
 public class Slot : MonoBehaviour
 {
+	private ColorSwitcher switcher;
+	private Animator animator;
 	public int index;
+
+	private void Awake() {
+		animator = GetComponent<Animator>();
+	}
+
 	public void OnClick() {
 		transform.parent.GetComponent<ColorSlotHolder>().ClickSlot(index);	
 	}
+
+	private void OnEnable() {
+		switcher = FindObjectOfType<ColorSwitcher>();
+		switcher.onColorSwitch += onColorSwitch;
+	}
+
+	private void OnDisable() {
+		if(switcher != null)
+			switcher.onColorSwitch -= onColorSwitch;
+		
+	}
+
+	void onColorSwitch(int newColor) {
+		animator.SetBool("currentColor", newColor == switcher.PickedColors[index]);
+	}
+
 }
